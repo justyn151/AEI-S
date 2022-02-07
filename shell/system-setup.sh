86a77436818b8ec69e44f7484669482d0a737b02
hctmate <62164966+justyn151@users.noreply.github.com>
2022-02-07 08:03:39 +0700

    Update and rename system-setup.sh to shell/system-setup.sh

--- shell/system-setup.sh
+++ shell/system-setup.sh
@@ -1,7 +1,17 @@
+#!/bin/bash
+
+echo "###########################################################"
+echo "# PLEASE SELECT THE DRIVE WHERE GRUB WANT TO BE INSTALLED #"
+echo "###########################################################"
+
 # drives
 fdisk -l
 drives=''
 read -p "Enter drives: " drives
+
+echo "#############"
+echo "# TIME ZONE #"
+echo "#############"
 
 # set timezone
 region=''
@@ -15,9 +25,17 @@ ln -sf /usr/share/zoneinfo/$region/$city /etc/localtime
 # hwclock
 hwclock --systohc
 
+echo "################"
+echo "# LOCALE SETUP #"
+echo "################"
+
 # localization
 echo "No support for now please do it manually"
 echo "Uncomment en_US.UTF-8 UTF-8"
+
+echo "################################"
+echo "# INSTALL YOUR FAVORITE EDITOR #"
+echo "################################"
 
 # installing the editors
 editors=''
@@ -30,6 +48,10 @@ $editors /etc/locale.gen
 
 # create locale.conf file
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+echo "#########################"
+echo "# USERNAME AND HOSTNAME #"
+echo "#########################"
 
 # set hostname
 hostname=''
@@ -47,8 +69,16 @@ useradd $user
 # set the new user password
 passwd $user
 
+echo "########################"
+echo "# ADDING USER TO GROUP #"
+echo "########################"
+
 # add user to group
 usermod -aG wheel,audio,video,optical,storage $user
+
+echo "###################"
+echo "# INSTALLING SUDO #"
+echo "###################"
 
 # editting the /etc/sudoers (instruction)
 echo "+ Uncomment the line that have %wheel%"
@@ -59,20 +89,40 @@ pacman -S sudo
 # openning the file
 EDITOR=vim visudo
 
+echo "###################"
+echo "# INSTALLING GRUB #"
+echo "###################"
+
 # installing grub
 pacman -S grub
+
+echo "########################"
+echo "# RUNNING GRUB-INSTALL #"
+echo "########################"
 
 # running grub-install
 grub-install $drives
 
+echo "#########################"
+echo "# RUNNING GRUB-MKCONFIG #"
+echo "#########################"
+
 # creating grub config
 grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "####################################"
+echo "# INSTALL AND ENABLE NEWORKMANAGER #"
+echo "####################################"
 
 # installing network manager
 pacman -S networkmanager
 
 # enabling the network manager
 systemctl enable NetworkManager
+
+echo "####################################"
+echo "# EXIT OUT FROM CHROOT ENVIRONMENT #"
+echo "####################################"
 
 # exit out from arch-chroot
 exit
